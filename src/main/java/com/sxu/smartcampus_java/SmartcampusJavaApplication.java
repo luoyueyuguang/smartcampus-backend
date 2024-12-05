d3a87168974991a05f3b55a2fdfa29e01c59f29e
luoyueyuguang <luoyuelight@outlook.com>
2024-12-05 09:24:37 +0800

    init

---
+++ src/main/java/com/sxu/smartcampus_java/SmartcampusJavaApplication.java
@@ -0,0 +1,13 @@
+package com.sxu.smartcampus_java;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartcampusJavaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SmartcampusJavaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sxu/smartcampus_java/SmartcampusJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sxu.smartcampus_java;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartcampusJavaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
